c59f99d496c2b8622049f5c3fd172da42eb22f30
awsarchitect2030 <awsarchitect2030@gmail.com>
2026-07-11 17:36:54 +0530

    Meta-Arguments

--- day04/main.tf
+++ day04/main.tf
@@ -1,3 +1,4 @@
+# Defines AWS S3 buckets using different methods: `count`, `depends_on`.
 resource "aws_s3_bucket" "my_bucket" {
   count = 3
   bucket = var.bucket_name[count.index]
@@ -15,9 +16,52 @@ resource "aws_s3_bucket" "my_bucket1" {
   tags = var.custom_tags
 }
 
+# Enabling bucket versioning for the S3 bucket created above.
+resource "aws_s3_bucket_versioning" "my_bucket_verssion"{
+  bucket = aws_s3_bucket.my_bucket1.id
+versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Defines AWS S3 buckets using different methods: for_each
 resource "aws_s3_bucket" "my_bucket_for_each" {
   for_each = var.bucket_name_for_each
   bucket = each.value
   
   tags = var.custom_tags
+}
+
+# create_before_destroy, prevent_destroy, ignore_changes example
+resource "aws_instance" "webserver" {
+  ami           = "ami-0d7b208dc5b0fe76a"
+  instance_type = "t3.micro"
+
+lifecycle {
+  create_before_destroy = true
+  prevent_destroy       = false # Set to true to prevent accidental deletion of the resource
+  ignore_changes        = [ami] # Ignore changes to the AMI attribute
+}
+}
+
+# precondition & postcondition example
+resource "aws_instance" "webserver_with_condition" {
+  ami           = "ami-0d7b208dc5b0fe76a"
+  instance_type = var.instance_type
+  tags          = var.tags
+
+# Below condition throws errors if instance type is not t3.micro
+  lifecycle {
+    precondition {
+      condition     = var.instance_type == "t3.micro"
+      error_message = "The instance type must be t3.micro."
+    }
+
+# Error: Resource postcondition failed message will be thrown if the Environment tag is not present in the tags map.
+      postcondition {
+      condition = contains(keys(self.tags), "Environment")
+      error_message = "The 'Environment' tag must be present in the tags."
+    }
+  
+  }
 }
--- day04/variables.tf
+++ day04/variables.tf
@@ -18,3 +18,18 @@ variable "bucket_name_for_each" {
     description = "List of S3 bucket names for for_each"
     default     = ["my-unique-bucket-name-11111", "my-unique-bucket-name-22222", "my-unique-bucket-name-33333"]
 }
+
+variable "instance_type" {
+    type        = string
+    description = "Instance type for the AWS instance"
+    default     = "t3.micro"
+}
+
+variable "tags" {
+    type        = map(string)
+    description = "Tags for the AWS instance"
+    default     = {
+        Name        = "MyInstance"
+        Environment = "Dev"
+    }
+}
